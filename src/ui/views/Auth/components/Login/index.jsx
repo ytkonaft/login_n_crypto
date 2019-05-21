21aee2a6b17cfb5de5a906cc7f1795baf28d8cd3
ytkonaft <boho.ufo@gmail.com>
2019-05-21 20:15:45 +0300

    formik is added

--- src/ui/views/Auth/components/Login/index.jsx
+++ src/ui/views/Auth/components/Login/index.jsx
@@ -1,43 +1,82 @@
 import React, { useState } from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 import './index.scss';
 
-const Signup = ({ handleLogin }) => {
-  const [userData, setData] = useState({
-    email: '',
-    password: ''
-  });
+const SignupSchema = Yup.object().shape({
+  password: Yup.string()
+    .min(6, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Required')
+});
 
-  const handleChange = ({ currentTarget: { name, value } }) => {
-    setData({
-      ...userData,
-      [name]: value
+const Signup = ({ handleLogin }) => {
+  const handleSubmit = (userData, { setStatus, resetForm }) => {
+    handleLogin(userData).catch((err) => {
+      setStatus(err.response.data.message);
     });
   };
 
-  const handleSubmit = () => {
-    handleLogin(userData);
+  const validatedFrom = touched => (Object.keys(touched).length ? 'was-validated' : '');
+
+  const fieldClass = (errors, touched, nameField) => {
+    if (errors[nameField] === undefined && touched[nameField] === undefined) {
+      return '';
+    }
+    if (errors[nameField] && touched[nameField] === true) {
+      return 'is-invalid';
+    }
+    return 'is-valid';
   };
 
   return (
-    <div>
-      <div className="login-form">
-        <h3>Login</h3>
-        <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          onChange={handleChange}
-          value={userData.email}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          onChange={handleChange}
-          value={userData.password}
-        />
-        <button onClick={handleSubmit}>Sign in</button>
-      </div>
+    <div className="login-form">
+      <Formik
+        initialValues={{
+          email: '',
+          password: ''
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ errors, touched, status }) => (
+          <Form className={`needs-validation ${validatedFrom(touched)}`}>
+            <div className="form-group">
+              <label>Email address</label>
+              <Field
+                type="email"
+                name="email"
+                className={fieldClass(errors, touched, 'email')}
+                placeholder="Enter email"
+              />
+
+              {errors.email && touched.email && (
+                <div className="invalid-feedback">{errors.email}</div>
+              )}
+            </div>
+
+            <div className="form-group">
+              <label>Password</label>
+              <Field
+                type="password"
+                name="password"
+                className={fieldClass(errors, touched, 'password')}
+                placeholder="Password"
+              />
+              {errors.password && touched.password && (
+                <div className="invalid-feedback">{errors.password}</div>
+              )}
+            </div>
+            {status && <div className="invalid-feedback">{status}</div>}
+            <button className="btn" type="submit">
+              LogIn
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
